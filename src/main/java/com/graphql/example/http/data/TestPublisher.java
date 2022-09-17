31f917bc180d537f82eb8159117b523b050c1695
qiongqiong.ge <qiongqiong.ge@pactera.com>
2022-09-17 19:21:01 +0800

    graphql-java Subscriptions over WebSockets example

--- src/main/java/com/graphql/example/http/data/TestPublisher.java
+++ src/main/java/com/graphql/example/http/data/TestPublisher.java
@@ -4,12 +4,14 @@ package com.graphql.example.http.data;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.FluxSink;
 
+import java.util.Random;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class TestPublisher {
     private final Flux<Integer> publisher;
+    private Random random = new Random();
 
     public TestPublisher() {
         publisher = Flux.create(emitter -> {
@@ -21,9 +23,7 @@ public class TestPublisher {
     }
 
     private Runnable newStockTick(FluxSink<Integer> emitter) {
-        return () -> {
-            emitter.next(1);
-        };
+        return () -> emitter.next(random.nextInt(10));
     }
 
     public Flux<Integer> getPublisher() {
